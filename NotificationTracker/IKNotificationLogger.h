9eef43910a5c29899699edde20cf6aab4d5ac856
Frédéric VERGEZ <fred@ikarius.com>
2014-02-13 18:18:28 +0100

    Added comments

--- NotificationTracker/IKNotificationLogger.h
+++ NotificationTracker/IKNotificationLogger.h
@@ -8,12 +8,38 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ * Simple convenience notification logger.
+ *
+ * You can add manually new notification names with the +trackNotification: method
+ * OR
+ * create an entry in the bundle dictionary under the key '' containing the name of a plist
+ * configuration file (without .plist extension).
+ *
+ * This file must contain a single array of notification keys/name.
+ *
+ * Feel free to replace NSLog by the logger of your choice.
+ *
+ * Initialize once with the +start method.
+ */
 @interface IKNotificationLogger : NSObject
 
-+ (instancetype)sharedInstance;
+/**
+ * Start logging notification content.
+ */
++ (void)start;
 
+/**
+ * Flush the current pool of tracked notifications.
+ * Reload config file if any.
+ */
++ (void)reset;
+
+/**
+ * Add a new notification (name) to track.
+ * notificationName: Name/key of the notification.
+ */
 + (void)trackNotification:(NSString*)notificationName;
 
-+ (void)reset;
 
 @end
